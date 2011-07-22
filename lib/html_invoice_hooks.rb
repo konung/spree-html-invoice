74d3dce26a55e994261c9486c7e7af40063b6311
Torsten Rüger <torsten@lightning.nu>
2011-07-22 18:20:41 +0300

    renames

--- lib/html_invoice_hooks.rb
+++
@@ -1,8 +0,0 @@
-class HtmlInvoiceHooks < Spree::ThemeSupport::HookListener
-
-
-  insert_after :admin_order_show_buttons , 'html_buttons'
-
-  insert_after :admin_order_edit_buttons , 'html_buttons'
-
-end
